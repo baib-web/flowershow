3b5784b28f77f0e5c56c9f312e18735c92fba5a8
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-28 15:44:23 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -59,7 +59,8 @@ module.exports = {
   },
   /* eslint global-require: off */
   plugins: [
-    require("@tailwindcss/typography"),
-    require('./plugins/image-responsive'),
+    require("@tailwindcss/typography"),//官方
+    require('./plugins/image-responsive'),//横滚图片宽度大小适应小屏幕
+    require('./hide-scrollbar'),//隐藏滚动条
   ],
 };
