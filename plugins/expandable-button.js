afe7137784638179ec2e5c3a11dfb512934fa32b
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-29 03:37:22 +0800

    Update expandable-button.js

--- plugins/expandable-button.js
+++ plugins/expandable-button.js
@@ -1,29 +1,22 @@
-module.exports = {
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        '.expandable-button': {
-          'display': 'block',
-          'width': '300px', // 按钮宽度
-          'padding': '10px',
-          'background-color': '#007BFF', // 按钮背景颜色
-          'color': 'white', // 按钮文字颜色
-          'text-align': 'center',
-          'border-radius': '5px', // 按钮圆角
-          'cursor': 'pointer',
-          'font-size': '16px',
-          'transition': 'background-color 0.3s',
-        },
-        '.expandable-button:hover': {
-          'background-color': '#0056b3', // 悬停时按钮背景颜色
-        },
-        'summary::marker': {
-          'display': 'none', // 隐藏默认的箭
-        },
-      });
+module.exports = function ({ addUtilities }) {
+  addUtilities({
+    '.expandable-button': {
+      'display': 'block',
+      'width': '300px', // 按钮宽度
+      'padding': '10px',
+      'background-color': '#007BFF', // 按钮背景颜色
+      'color': 'white', // 按钮文字颜色
+      'text-align': 'center',
+      'border-radius': '5px', // 按钮圆角
+      'cursor': 'pointer',
+      'font-size': '16px',
+      'transition': 'background-color 0.3s',
     },
-  ],
+    '.expandable-button:hover': {
+      'background-color': '#0056b3', // 悬停时按钮背景颜色
+    },
+    'summary::marker': {
+      'display': 'none', // 隐藏默认的箭头
+    },
+  });
 };
