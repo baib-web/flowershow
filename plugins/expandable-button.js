dbdb8d46556d6ffa47c691d352dbc48ac7fa99d5
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-29 03:54:04 +0800

    Update expandable-button.js

--- plugins/expandable-button.js
+++ plugins/expandable-button.js
@@ -2,18 +2,17 @@ module.exports = function ({ addUtilities }) {
   addUtilities({
     '.expandable-button': {
       'display': 'block',
-      'width': '300px', // 按钮宽度
+      'width': '100%', // 按钮宽度
       'padding': '10px',
       'background-color': 'transparent', // 按钮背景颜色，完全透明
       'color': 'white', // 按钮文字颜色
       'text-align': 'center',
-      'border-radius': '5px', // 按钮圆角
+      'border-radius': '0px', // 按钮圆角
       'cursor': 'pointer',
       'font-size': '16px',
       'transition': 'background-color 0.3s',
     },
     '.expandable-button:hover': {
-      'background-color': 'rgba(0, 86, 179, 0.5)', // 悬停时按钮背景颜色，带透明度
     },
     'summary::marker': {
       'display': 'none', // 隐藏默认的箭头
