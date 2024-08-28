db921910b4152790fb81177215133f9b8ab38481
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-29 02:45:34 +0800

    Update glow-text.js

--- plugins/glow-text.js
+++ plugins/glow-text.js
@@ -2,10 +2,10 @@ module.exports = function ({ addUtilities }) {
   addUtilities({
     '.glow': {
       'color': '#FFFFFF', // 设置文字颜色为纯白色
-      'text-shadow': '0 0 10px var(--glow-color), 0 0 20px var(--glow-color)',
-      'filter': 'blur(0.5px)',
+      'text-shadow': '0 0 5px var(--glow-color), 0 0 10px var(--glow-color)',
+      'filter': 'blur(0.3px)',
       '--glow-color': 'rgba(255, 255, 255, 0.6)',
-      '--glow-blur': '0.5px'
+      '--glow-blur': '0.3px'
     }
   })
 }
