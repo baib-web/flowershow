5c72f8fad6eb3fa7d89d6d2bfa558ad95a0ae110
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-28 15:42:17 +0800

    Create hide-scrollbar.js

---
+++ plugins/hide-scrollbar.js
@@ -0,0 +1,12 @@
+module.exports = function ({ addUtilities }) {
+  addUtilities({
+    '.hide-scrollbar': {
+      /* Chrome, Edge, Safari */
+      '-webkit-overflow-hidden': 'hidden',
+      /* Firefox */
+      '-moz-overflow-hidden': 'hidden',
+      /* IE, Edge */
+      'overflow': 'hidden',
+    },
+  })
+}
