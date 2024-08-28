bc14e53bab4602b6dac1af38f908963c76d64b3e
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-28 12:00:05 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -53,5 +53,16 @@ module.exports = {
   /* eslint global-require: off */
   plugins: [
     require("@tailwindcss/typography")
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',     /* Firefox */
+          '&::-webkit-scrollbar': {
+            display: 'none',             /* Chrome, Safari, and Opera */
+          },
+        },
+      });
+    }),
   ],
 };
