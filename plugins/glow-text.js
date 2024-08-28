20a57f88ecab88cdd1c2aced2dbe3ce80b568132
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-28 16:03:04 +0800

    Create glow-text.js

---
+++ plugins/glow-text.js
@@ -0,0 +1,10 @@
+module.exports = function ({ addUtilities }) {
+  addUtilities({
+    '.glow': {
+      'text-shadow': '0 0 5px var(--glow-color), 0 0 10px var(--glow-color)',
+      'filter': 'blur(var(--glow-blur))',
+      '--glow-color': 'rgba(255, 255, 255, 0.3)',
+      '--glow-blur': '1px'
+    }
+  })
+}
