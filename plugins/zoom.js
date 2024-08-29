6060a6042c61095f17e1cb251198c77e5e01f67f
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-29 22:57:22 +0800

    Update zoom.js

--- plugins/zoom.js
+++ plugins/zoom.js
@@ -1,8 +1,6 @@
 const iframe = document.getElementById('my-iframe');
 iframe.addEventListener('wheel', (event) => {
   event.preventDefault();
-  // 在这里添加自定义的缩放逻辑，比如根据鼠标滚轮的方向进行缩放
-  // 可以使用 Tailwind CSS 的类名来控制缩放
   if (event.deltaY > 0) {
     iframe.classList.add('scale-125');
     iframe.classList.remove('scale-100');
