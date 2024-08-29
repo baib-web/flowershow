bf05560f07db58571a0eb1bf3b85e68a34af72c3
baib-web <55641681+baib-web@users.noreply.github.com>
2024-08-30 02:12:47 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,6 +24,13 @@ module.exports = {
           '100%': { backgroundPosition: '0% 50%' },
         },
       },
+      // bilibili
+      container: {
+        'iframe-container': {
+          position: 'relative',
+          padding: '30% 45%',
+        },
+      },
       // support wider width for large screens >1440px eg. in hero
       maxWidth: {
         "8xl": "88rem",
